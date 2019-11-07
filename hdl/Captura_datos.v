3030b40f1e44e91c38d54cba7e5acdaccbad8492
MitchellElizabethRodriguezBarreto <mierodriguezba@unal.edu.co>
2019-11-07 17:42:13 -0500

    Avance captura de datos

---
+++ hdl/Captura_datos.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:07:03 11/07/2019 
+// Design Name: 
+// Module Name:    Captura_datos 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Captura_datos(
+	input PCLK,
+	input HREF,
+	input VSYNC,
+	input D0,
+	input D1,
+	input D2,
+	input D3,
+	input D4,
+	input D5,
+	input D6,
+	input D7,
+	output DP_RAM_data_in[7:0],
+	output DP_RAM_addr_in,
+	output DP_RAM_regW
+    );
+
+	reg cont=1'b0;
+	reg color[7:0];
+		
+	always@(posedge(PCLK and HREF and ~VSYNC))
+	
+	begin 
+		color[7:0]={D0,D1,D2,D3,D4,D5,D6,D7};
+		if (cont==0)
+		begin
+			DP_RAM_data_in={color[7:5],color[2:0],DP_RAM_data_in[1:0]};
+			DP_RAM_regW=0;
+		end
+		else 
+		begin
+			DP_RAM_data_in={DP_RAM_data_in[7:2],color[4:3]};
+			DP_RAM_regW=1;
+		end
+		cont = cont+1;
+	end
+	
+	always@(negedge(PCLK and VSYNC and ~HREF))
+	begin
+		addr_in = addr_in+1;
+	end
+	
+endmodule
--- hdl/src/test_cam.v
+++ hdl/src/test_cam.v
@@ -19,7 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module test_cam(
-    input wire clk,           // board clock: 32 MHz 
+    input wire clk,           // board clock: 100 MHz 
     input wire rst,         	// reset button
 
 	// VGA input/output  
@@ -38,11 +38,11 @@ module test_cam(
 
 );
 
-// TAMAÑO DE ADQUISICIÓN DE LA CAMARA 
-parameter CAM_SCREEN_X = 160;
-parameter CAM_SCREEN_Y = 120;
+// TAMANNO DE ADQUISICION DE LA CAMARA 
+parameter CAM_SCREEN_X = 320;
+parameter CAM_SCREEN_Y = 240;
 
-localparam AW = 15; // LOG2(CAM_SCREEN_X*CAM_SCREEN_Y)
+localparam AW = 17; // LOG2(CAM_SCREEN_X*CAM_SCREEN_Y)
 localparam DW = 8;
 
 // El color es RGB 332
@@ -52,11 +52,11 @@ localparam BLUE_VGA =  8'b00000011;
 
 
 // Clk 
-wire clk32M;
+wire clk100M;
 wire clk25M;
 wire clk24M;
 
-// Conexión dual por ram
+// Conexion dual por ram
 
 wire  [AW-1: 0] DP_RAM_addr_in;  
 wire  [DW-1: 0] DP_RAM_data_in;
@@ -64,7 +64,7 @@ wire DP_RAM_regW;
 
 reg  [AW-1: 0] DP_RAM_addr_out;  
 	
-// Conexión VGA Driver
+// Conexion VGA Driver
 wire [DW-1:0]data_mem;	   // Salida de dp_ram al driver VGA
 wire [DW-1:0]data_RGB332;  // salida del driver VGA al puerto
 wire [9:0]VGA_posX;		   // Determinar la pos de memoria que viene del VGA
@@ -82,7 +82,7 @@ assign VGA_B = {data_RGB332[1:0],2'b00};
 
 
 /* ****************************************************************************
-Asignación de las señales de control xclk pwdn y reset de la camara 
+Asignacin de las seales de control xclk pwdn y reset de la camara 
 **************************************************************************** */
 
 assign CAM_xclk=  clk24M;
@@ -92,7 +92,7 @@ assign CAM_reset=  0;
 
 
 /* ****************************************************************************
-  Este bloque se debe modificar según sea le caso. El ejemplo esta dado para
+  Este bloque se debe modificar segn sea le caso. El ejemplo esta dado para
   fpga Spartan6 lx9 a 32MHz.
   usar "tools -> Core Generator ..."  y general el ip con Clocking Wizard
   el bloque genera un reloj de 25Mhz usado para el VGA  y un relo de 24 MHz
@@ -110,7 +110,7 @@ clk_32MHZ_to_25M_24M
 
 /* ****************************************************************************
 buffer_ram_dp buffer memoria dual port y reloj de lectura y escritura separados
-Se debe configurar AW  según los calculos realizados en el Wp01
+Se debe configurar AW  segn los calculos realizados en el Wp01
 se recomiendia dejar DW a 8, con el fin de optimizar recursos  y hacer RGB 332
 **************************************************************************** */
 buffer_ram_dp #( AW,DW)
@@ -135,18 +135,18 @@ VGA_Driver640x480 VGA640x480
 	.clk(clk25M), 				// 25MHz  para 60 hz de 640x480
 	.pixelIn(data_mem), 		// entrada del valor de color  pixel RGB 332 
 	.pixelOut(data_RGB332), // salida del valor pixel a la VGA 
-	.Hsync_n(VGA_Hsync_n),	// señal de sincronizaciÓn en horizontal negada
-	.Vsync_n(VGA_Vsync_n),	// señal de sincronizaciÓn en vertical negada 
-	.posX(VGA_posX), 			// posición en horizontal del pixel siguiente
-	.posY(VGA_posY) 			// posición en vertical  del pixel siguiente
+	.Hsync_n(VGA_Hsync_n),	// seal de sincronizacin en horizontal negada
+	.Vsync_n(VGA_Vsync_n),	// seal de sincronizacin en vertical negada 
+	.posX(VGA_posX), 			// posicin en horizontal del pixel siguiente
+	.posY(VGA_posY) 			// posicin en vertical  del pixel siguiente
 
 );
 
  
 /* ****************************************************************************
-LÓgica para actualizar el pixel acorde con la buffer de memoria y el pixel de 
+Lgica para actualizar el pixel acorde con la buffer de memoria y el pixel de 
 VGA si la imagen de la camara es menor que el display  VGA, los pixeles 
-adicionales seran iguales al color del último pixel de memoria 
+adicionales seran iguales al color del ltimo pixel de memoria 
 **************************************************************************** */
 always @ (VGA_posX, VGA_posY) begin
 		if ((VGA_posX>CAM_SCREEN_X-1) || (VGA_posY>CAM_SCREEN_Y-1))
