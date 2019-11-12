6380e70c78f0fc1ebaa191d82a857bc6a4f6fc12
MitchellElizabethRodriguezBarreto <mierodriguezba@unal.edu.co>
2019-11-12 11:58:25 -0500

    clk_100MHZ_to_25M_24M

--- hdl/src/test_cam.v
+++ hdl/src/test_cam.v
@@ -103,7 +103,7 @@ assign CAM_reset = 0;
   utilizado para la camara , a partir de una frecuencia de 32 Mhz
 **************************************************************************** */
 //assign clk32M =clk;
-clk_32MHZ_to_25M_24M
+clk_100MHZ_to_25M_24M
   clk25_24(
   .CLK_IN1(clk),
   .CLK_OUT1(clk25M),
